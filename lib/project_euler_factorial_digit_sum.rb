b200d0a4da465957878a8c0443cd0afae30fa185
gregmarquet <marquet.greg@gmail.com>
2016-11-21 16:48:06 -0500

    Done

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,15 @@
-# code your solution here
+def factorial(num)
+  if (num == 0 || num == 1)
+    1
+  else
+    num * factorial(num - 1)
+  end
+end
+
+def sum_of_digits(num)
+  num.to_s.chars.map(&:to_i).reduce(:+)
+end
+
+def factorial_digit_sum(num)
+  sum_of_digits(factorial(num))
+end
